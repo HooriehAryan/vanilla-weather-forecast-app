d8975402c82ab879c916de7f3bcf6ce0aca18fe2
H <ha8086@gmail.com>
2023-04-25 21:22:48 +0430

    add forecast for 5 days

--- src/index.js
+++ src/index.js
@@ -37,52 +37,86 @@ function formatDate(timestamp) {
   return `${day}, ${today} ${month} ${hours}:${minutes}`;
 }
 
-//------------------convert dt in day format------------------
+//----------------------convert dt in day format-----------------------
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
-
-//-------------------- display forecast--------------------------
-function displayForecast(response) {
-  console.log(response);
+//---------------convert tempreture for forecast data-------------------
+function displayFarenHietTempForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = "";
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
-      forecastHTML =
-        forecastHTML +
-        `<div class="col-2">
-      <p>${formatDay(forecastDay.dt)}</p>
+  for (let i = 1; i < 6; i++) {
+    forecast = response.data.daily[i];
+
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+      <p>${formatDay(forecast.dt)}</p>
       
       <p>
          <img class="icon" src="http://openweathermap.org/img/wn/${
-           forecastDay.weather[0].icon
+           forecast.weather[0].icon
          }@2x.png" alt="" >
       </p>
           <p>
         <span class="weather-forecas-temp-max">${Math.round(
-          forecastDay.temp.max
+          (forecast.temp.max * 9) / 5 + 32
         )}</span>°/
         <span class="weather-forecas-temp-min">${Math.round(
-          forecastDay.temp.min
+          (forecast.temp.min * 9) / 5 + 32
+        )}</span>°
+      </p>
+    </div>
+    
+  `;
+  }
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+//-------------------- display forecast--------------------------
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = "";
+  let forecastHTML = `<div class="row">`;
+
+  for (let i = 1; i < 6; i++) {
+    forecast = response.data.daily[i];
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+      <p>${formatDay(forecast.dt)}</p>
+      
+      <p>
+         <img class="icon" src="http://openweathermap.org/img/wn/${
+           forecast.weather[0].icon
+         }@2x.png" alt="" >
+      </p>
+          <p>
+        <span class="weather-forecas-temp-max">${Math.round(
+          forecast.temp.max
+        )}</span>°/
+        <span class="weather-forecas-temp-min">${Math.round(
+          forecast.temp.min
         )}</span>°
       </p>
     </div>
   `;
-    }
-  });
-
+  }
+  // })
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 //----------------get coodinates for forecast weather-------------------
 function getForecast(coordinates) {
-  console.log(coordinates);
+  lat = coordinates.lat;
+  long = coordinates.lon;
   let key = "97bed167ec49bff56e6c1b63daef9c86";
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
   console.log(url);
@@ -142,10 +176,10 @@ function displayCurrentWeather(response) {
 
 //----search current location with latitude and longitude-------
 function showPosition(position) {
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
   let key = "cff65853d7c461490797b173c0cc1233";
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
   let searchInput = document.querySelector("#search-input");
   searchInput.value = "";
   axios.get(url).then(displayCurrentWeather);
@@ -162,11 +196,12 @@ function searchCity(city) {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
   axios.get(url).then(displayCurrentWeather);
 }
-
+//-----------show city title then call a fuction for search the weasther values----------
 function cityTitle(event) {
   event.preventDefault();
 
   let newCity = document.querySelector("#search-input").value;
+  city = newCity;
 
   if (newCity.length === 0) {
     alert("Enter a city name");
@@ -174,13 +209,15 @@ function cityTitle(event) {
     searchCity(newCity);
   }
 }
+
 //---------------convert tempreture-------------------
 function displayFarenHietTemp(event) {
   event.preventDefault();
-  let tempElement = document.querySelector("#current-temp");
   //remove the active class from cel
   celsiusLink.classList.remove("active");
   farenhietLink.classList.add("active");
+
+  let tempElement = document.querySelector("#current-temp");
   farenhietTemp = (celsiusTemp * 9) / 5 + 32;
   tempElement.innerHTML = Math.round(farenhietTemp);
 
@@ -193,6 +230,13 @@ function displayFarenHietTemp(event) {
   document.querySelector("#feelsLike").innerHTML = Math.round(
     (feelsLikeTemp * 9) / 5 + 32
   );
+
+  //call a function whitch convert cel to faren for an array
+  let key = "97bed167ec49bff56e6c1b63daef9c86";
+  let latitude = lat;
+  let longitude = long;
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
+  axios.get(url).then(displayFarenHietTempForecast);
 }
 
 function displayCelsiusTemp(event) {
@@ -204,17 +248,23 @@ function displayCelsiusTemp(event) {
   document.querySelector("#maxTemp").innerHTML = Math.round(maxTemp);
   document.querySelector("#minTemp").innerHTML = Math.round(minTemp);
   document.querySelector("#feelsLike").innerHTML = Math.round(feelsLikeTemp);
+
+  searchCity(city);
 }
 //-----------------------------------------------------
-//---set globale variable----
+//---set global variables----
+city = "San Diego";
 let celsiusTemp = null;
 let feelsLikeTemp = null;
 let maxTemp = null;
 let minTemp = null;
-
+let lat = 0;
+let long = 0;
 //---show default city values--
-searchCity("San Diego");
-
+searchCity(city);
+window.onload = function () {
+  // getForecast(coordinates);
+};
 //---click search button--
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", cityTitle);
